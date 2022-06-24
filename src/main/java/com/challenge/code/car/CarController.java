4ca82e181ec9042ec6e97298782ac85f58d0170d
Marialejandra Contreras <34869971+matycarolina@users.noreply.github.com>
2022-06-23 20:50:53 -0500

    Add dockerfile

--- src/main/java/com/challenge/code/car/CarController.java
+++ src/main/java/com/challenge/code/car/CarController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/v1/car")
+@RequestMapping(path = "/api/v1/car")
 public class CarController {
 
     private final CarService carService;
